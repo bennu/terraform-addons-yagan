e1c4037b3506cbf5ac369c006bc94dee2fa102e4
Pierre Ugaz <ulm0@innersea.xyz>
2020-10-28 16:46:21 -0300

    chore: add klum to addons

---
+++ klum.tf
@@ -0,0 +1,93 @@
+# https://raw.githubusercontent.com/ibuildthecloud/klum/master/deploy.yaml
+
+resource kubernetes_service_account klum {
+  count = local.enable_klum ? 1 : 0
+  metadata {
+    name      = "klum"
+    namespace = "kube-system"
+    labels = {
+      "app" = "klum"
+    }
+  }
+
+  automount_service_account_token = true
+}
+
+resource kubernetes_cluster_role_binding klum {
+  count = local.enable_klum ? 1 : 0
+  metadata {
+    name = "klum-cluster-admin"
+    labels = {
+      "app" = "klum"
+    }
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.klum.0.metadata.0.name
+    namespace = kubernetes_service_account.klum.0.metadata.0.namespace
+  }
+}
+
+resource kubernetes_deployment klum {
+  count = local.enable_klum ? 1 : 0
+  metadata {
+    name      = "klum"
+    namespace = "kube-system"
+    labels = {
+      "app" = "klum"
+    }
+  }
+
+  spec {
+    selector {
+      match_labels = {
+        "app" = "klum"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          "app" = "klum"
+        }
+      }
+
+      spec {
+        automount_service_account_token = true
+        priority_class_name             = "system-cluster-critical"
+        service_account_name            = kubernetes_service_account.klum.0.metadata.0.name
+        affinity {
+          node_affinity {
+            required_during_scheduling_ignored_during_execution {
+              node_selector_term {
+                match_expressions {
+                  key      = "node-role.kubernetes.io/controlplane"
+                  operator = "Exists"
+                }
+              }
+            }
+          }
+        }
+
+        toleration {
+          effect   = "NoSchedule"
+          key      = "node-role.kubernetes.io/controlplane"
+          operator = "Equal"
+          value    = "true"
+        }
+
+        container {
+          image = local.klum_image
+          name  = "klum"
+        }
+      }
+    }
+  }
+}
